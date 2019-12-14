2e1c297edad11f35009ad0034dac05159ad42af7
Destiny <228902448@qq.com>
2019-12-14 14:44:29 +0800

    swiper

--- travel/src/main.js
+++ travel/src/main.js
@@ -4,14 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
+/*vue-awesome-swiper用于轮播图效果*/
 /* border.css解决多倍屏幕1px问题 */
 /* 部分移动端机型使用click存在300ms延迟事件,引入fastClick解决*/
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt=""/>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "HomeSwiper",
+    data: function () {
+      return {
+        swiperOption: {
+          pagination: '.swiper-pagination',
+          loop: true
+        },
+        swiperList: [{
+          id: '001',
+          imgUrl: 'http://e.hiphotos.baidu.com/image/pic/item/359b033b5bb5c9eac1754f45df39b6003bf3b396.jpg'
+        }, {
+          id: '002',
+          imgUrl: 'http://b.hiphotos.baidu.com/image/pic/item/908fa0ec08fa513db777cf78376d55fbb3fbd9b3.jpg'
+        }]
+      }
+    },
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #ffffff !important
+
+  .wrapper
+    width 100%
+    height 0
+    overflow hidden
+    padding-bottom 31.25%
+
+
+    .swiper-img
+      width 100%
+
+</style>
