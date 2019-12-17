9d47b7b4c98c00dd190aeaaa5d30885907184405
Destiny <228902448@qq.com>
2019-12-17 16:14:07 +0800

    perfomace

--- travel/src/pages/city/City.vue
+++ travel/src/pages/city/City.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list :cities="cities" :hotCities="hotCities"></city-list>
-    <city-alphabet :cities="cities"></city-alphabet>
+    <city-list :cities="cities" :hotCities="hotCities" :letter="letter"></city-list>
+    <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
   </div>
 </template>
 
@@ -20,6 +20,7 @@
       return {
         cities: {},
         hotCities: [],
+        letter: '',
       }
     },
     components: {
@@ -33,13 +34,16 @@
         axios.get('/api/city.json').then(this.getCityInfoSucc)
       },
       getCityInfoSucc(res) {
-        console.log(res)
+        // console.log(res)
         res = res.data
         if (res.ret && res.data) {
           const data = res.data;
           this.cities = data.cities;
           this.hotCities = data.hotCities;
         }
+      },
+      handleLetterChange(letter) {
+        this.letter = letter;
       }
     },
     mounted() {
--- travel/src/pages/city/components/Alphabet.vue
+++ travel/src/pages/city/components/Alphabet.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <ul class="list">
-      <li class="item" v-for="(item,key) of cities" :key="key">{{key}}</li>
+      <li class="item"
+          v-for="item of letters"
+          :key="item"
+          :ref="item"
+          @touchstart="handleTouchStart"
+          @touchmove="handleTouchMove"
+          @touchend="handleTouchEnd"
+          @click="handleLetterClick">{{item}}
+      </li>
     </ul>
   </div>
 </template>
@@ -12,6 +20,54 @@
     props: {
       cities: Object
     },
+    computed: {
+      letters() {
+        const letters = []
+        for (let i in this.cities) {
+          letters.push(i)
+        }
+        return letters
+      }
+    },
+    data() {
+      return {
+        touchStatus: false,
+        startY: 0,
+        // 函数节流
+        timer: null,
+      }
+    },
+    //当页面填充数据的时候进行计算，优化性能
+    updated() {
+      this.startY = this.$refs['A'][0].offsetTop
+    },
+    methods: {
+      handleLetterClick(e) {
+        this.$emit('change', e.target.innerText);
+      },
+      handleTouchStart() {
+        this.touchStatus = true
+      },
+      handleTouchMove(e) {
+        if (this.touchStatus) {
+          //检测是否在操作，如果在操作则延迟16ms操作，如果在16ms中继续滚动则清除重新操作
+          if (this.timer) {
+            clearTimeout(this.timer)
+          }
+          this.title = setTimeout(() => {
+            const touchY = e.touches[0].clientY - 79
+            const index = Math.floor((touchY - this.startY) / 20)
+            if (index >= 0 && index < this.letters.length) {
+              this.$emit('change', this.letters[index])
+            }
+          }, 16)
+        }
+      },
+      handleTouchEnd() {
+        this.touchStatus = false
+      },
+
+    }
   }
 </script>
 
