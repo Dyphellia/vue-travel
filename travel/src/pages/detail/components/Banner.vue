a60fe58a3e32b854c4ad107804f29ab95ec0f633
Destiny <228902448@qq.com>
2019-12-24 15:10:07 +0800

    animation end

--- travel/src/pages/detail/components/Banner.vue
+++ travel/src/pages/detail/components/Banner.vue
@@ -7,20 +7,23 @@
         <div class="banner-number"><span class="iconfont banner-icon">&#xe6ac;</span>{{this.gallaryImgs.length}}</div>
       </div>
     </div>
-    <common-gallary :gallaryImgs="gallaryImgs" v-show="showGallary" @close="hanldeGallaryClose"></common-gallary>
+    <fade-animation>
+      <common-gallary :imgs="gallaryImgs" v-show="showGallary" @close="hanldeGallaryClose"></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 
   import CommonGallary from 'common/gallary/Gallary'
+  import FadeAnimation from 'common/fade/FadeAnimation'
 
   export default {
     name: "DetailBanner",
-    props:{
-      sightName:String,
-      bannerImg:String,
-      gallaryImgs:Array,
+    props: {
+      sightName: String,
+      bannerImg: String,
+      gallaryImgs: Array,
     },
     data() {
       return {
@@ -37,6 +40,7 @@
     },
     components: {
       CommonGallary,
+      FadeAnimation
     },
   }
 </script>
